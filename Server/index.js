de8cd3094a8da3e5230b7bbe5fc432b1ebb17276
Akash Patel <patelakash1627@gmail.com>
2024-11-11 07:58:35 +0530

    cors handled

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
@@ -12,6 +13,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
